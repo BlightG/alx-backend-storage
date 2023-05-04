70b1c6bdb5f1baa723e4e105f8522619ee29aba6
BlightG <birhane.gulilat@gmail.com>
2023-05-04 14:23:57 +0300

    modified defualt syntax

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email CHAR(255) NOT NULL UNIQUE,
     name CHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT('US')
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );

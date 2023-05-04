7a5e9f62f8144fcade4b8d0d0a0717e276bc7d06
BlightG <birhane.gulilat@gmail.com>
2023-05-04 22:38:35 +0300

    task 6 second attempt

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,10 +2,14 @@
 DELIMITER //
 CREATE PROCEDURE AddBonus (IN user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
+    DECLARE project_id INT;
+    IF ((SELECT COUNT(*) AS num_rows FROM projects WHERE projects.name = project_name) = 0) THEN 
+        INSERT INTO `projects`(`name`)
+        VALUE (project_name);
+    END IF;
+    
+    SELECT projects.id INTO project_id FROM projects WHERE projects.name = project_name;
     INSERT INTO `corrections`(`user_id`, `project_id`, `score`)
-    SELECT user_id, projects.id, score
-    FROM projects
-    JOIN corrections ON projects.id = project_id
-    WHERE projects.name = project_name;
+    VALUES (user_id, project_id, score);
 END //
 DELIMITER ;

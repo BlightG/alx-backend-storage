3f667eb660e1f7b13f1ef13ab6c37144aa7e8059
BlightG <birhane.gulilat@gmail.com>
2023-05-05 19:52:15 +0300

    trying agin

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -2,4 +2,4 @@
 -- ALTER TABLE names
 -- ADD COLUMN first_letter VARCHAR(1) AS (SUBSTRING(name, 1, 1)) STORED;
 -- CREATE INDEX idx_name_first ON names (first_letter);
-CREATE INDEX idx_name_first ON names (name);
+CREATE INDEX idx_name_first ON names (SUBSTR(name, 1, 1));

551ac48e4bf493755c0b86de1d7b1680a46713bc
BlightG <birhane.gulilat@gmail.com>
2023-05-06 10:29:12 +0300

    reduced documentation

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,6 +1,2 @@
 -- indexed a list
--- ALTER TABLE names
--- ADD COLUMN first_letter VARCHAR(1) AS (SUBSTRING(name, 1, 1)) STORED;
--- CREATE INDEX idx_name_first ON names (first_letter);
--- CREATE INDEX idx_name_first ON names (name(1));
 ALTER TABLE names ADD INDEX idx_name_first (name(1));
